cf78fd00e37e76bd691f396f166ced7d34721875
Benjamin Bach <benjaoming@gmail.com>
2015-02-19 10:00:42 +0200

    Add manager pub keys and fix login, logout and lightdm configurations for 14.04

--- mzuni_student_lab/postinstall/filesystem/etc/lightdm/onlogin.sh
+++ mzuni_student_lab/postinstall/filesystem/etc/lightdm/onlogin.sh
@@ -4,5 +4,10 @@ echo $USER > /tmp/lastuser
 
 if [ "$USER" = "online" ]
 then
-	/etc/init.d/networking restart
+	# This only worked in 12.04 then all the fun
+	# of restarting all network interfaces went
+	# away :(
+	# /etc/init.d/networking restart
+	ifdown eth0
+	ifup eth0
 fi
